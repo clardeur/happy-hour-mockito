d043d730ea5cd31bde05ca6f15ac18ec9ca3d9fd
clardeur <clem.lardeur@gmail.com>
2013-01-28 19:36:44 +0100

    add BDD test example

--- src/test/java/fr/xebia/blog/OrderTest.java
+++ src/test/java/fr/xebia/blog/OrderTest.java
@@ -10,6 +10,7 @@ import java.util.Locale;
 
 import static com.google.common.collect.Lists.newArrayList;
 import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
 /**
@@ -22,7 +23,7 @@ public class OrderTest {
     @Mock Product product2;
 
     @Test
-    public void should_get_order_total_price() throws Exception {
+    public void should_have_a_total_price_equal_to_8_99() throws Exception {
         when(product1.getPrice()).thenReturn(new BigDecimal("3.99"));
         when(product2.getPrice()).thenReturn(new BigDecimal("5.00"));
         Order order = new Order(newArrayList(product1, product2));
@@ -30,15 +31,24 @@ public class OrderTest {
     }
 
     @Test
-    public void should_format_total_price_with_us_locale() throws Exception {
+    public void should_have_a_total_price_equals_to_20() throws Exception {
+        Order order = new Order(newArrayList(product1, product2));
+        given(product1.getPrice()).willReturn(BigDecimal.TEN); // GIVEN
+        given(product2.getPrice()).willReturn(BigDecimal.TEN);
+        BigDecimal totalPrice = order.getTotalPrice();         // WHEN
+        assertThat(totalPrice).isEqualTo(new BigDecimal(20));  // THEN
+    }
+
+    @Test
+    public void should_format_total_price_to_10_00_€_string() throws Exception {
         Order order = spy(new Order(null));
         doReturn(BigDecimal.TEN).when(order).getTotalPrice();
-        assertThat(order.formatTotalPrice(Locale.US)).isEqualTo("$10.00");
+        assertThat(order.formatTotalPrice(Locale.FRANCE)).isEqualTo("10,00 €");
         verify(order, times(1)).getTotalPrice();
     }
 
     @Test
-    public void should_return_empty_string_if_any_exception_occurs() throws Exception {
+    public void should_have_an_empty_string_if_any_exception_occurs() throws Exception {
         Order order = spy(new Order(null));
         doThrow(Exception.class).when(order).getTotalPrice();
         assertThat(order.formatTotalPrice(Locale.US)).isEmpty();
