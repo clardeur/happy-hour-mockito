cee80d256aa6081ea529569ff4c9619cf7ef7788
clardeur <clem.lardeur@gmail.com>
2013-01-30 18:21:39 +0100

    add OrderBDDTest

--- src/test/java/fr/xebia/blog/OrderTest.java
+++ src/test/java/fr/xebia/blog/OrderTest.java
@@ -1,41 +1,35 @@
 package fr.xebia.blog;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
 
 import java.math.BigDecimal;
 import java.util.Locale;
 
 import static com.google.common.collect.Lists.newArrayList;
 import static org.fest.assertions.Assertions.assertThat;
-import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
 /**
  * @author <a href="mailto:clardeur@xebia.fr">Clement Lardeur</a>
  */
-@RunWith(MockitoJUnitRunner.class)
 public class OrderTest {
-
-    @Mock Product product1;
-    @Mock Product product2;
 
     @Test
     public void should_have_a_total_price_equal_to_8_99() throws Exception {
+        Product product1 = mock(Product.class);
+        Product product2 = mock(Product.class);
         when(product1.getPrice()).thenReturn(new BigDecimal("3.99"));
         when(product2.getPrice()).thenReturn(new BigDecimal("5.00"));
         Order order = new Order(newArrayList(product1, product2));
         assertThat(order.getTotalPrice()).isEqualTo(new BigDecimal("8.99"));
     }
 
-    @Test
-    public void should_have_a_total_price_equals_to_20() throws Exception {
+    @Test(expected = IllegalStateException.class)
+    public void should_throws_exception_when_calling_get_price() throws Exception {
+        Product product1 = mock(Product.class);
+        when(product1.getPrice()).thenThrow(new IllegalStateException());
         Order order = new Order(newArrayList(product1));
-        given(product1.getPrice()).willReturn(BigDecimal.TEN); // GIVEN
-        BigDecimal totalPrice = order.getTotalPrice();         // WHEN
-        assertThat(totalPrice).isEqualTo(BigDecimal.TEN);      // THEN
+        order.getTotalPrice();
     }
 
     @Test
@@ -44,12 +38,5 @@ public class OrderTest {
         doReturn(BigDecimal.TEN).when(order).getTotalPrice();
         assertThat(order.formatTotalPrice(Locale.FRANCE)).isEqualTo("10,00 €");
         verify(order, times(1)).getTotalPrice();
-    }
-
-    @Test
-    public void should_have_an_empty_string_if_any_exception_occurs() throws Exception {
-        Order order = spy(new Order(null));
-        doThrow(Exception.class).when(order).getTotalPrice();
-        assertThat(order.formatTotalPrice(Locale.US)).isEmpty();
     }
 }
